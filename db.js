bcfd69f64dc76883fdc4e7a7b755ee5a61b42a47
Asim Riaz <asim.riaz@live.com>
2024-05-03 00:44:39 +0500

    Changes

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import sql from 'mssql'
 
 const config = {
     user: 'sa',
-    password: 'Welcome@494',
+    password: 'Welcome@123',
     server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
     database: 'Silberschatz',
     synchronize: true,
--- public/app.js
+++ public/app.js
@@ -1,19 +1,19 @@
-function dataD() {
+function App() {
     return {
-        page: 1, 
+        page: 1,
         courses: [],
         dept: {},
-        async getCourses(){
-            const response = (await axios.get('/api/courses')).data
+        async getCourses() {
+            const courses = await fetch('/api/courses').then(res => res.json())
             //console.log(response)
-            this.courses = response; 
+            this.courses = courses;
         },
-        async getDepartment(dept_name){
+        async getDepartment(dept_name) {
             console.log(dept_name)
-            const response = (await axios.get(`/api/department/${dept_name}`)).data
+            const dept = await fetch(`/api/department/${dept_name}`).then(res => res.json())
             this.page = 2
-            this.dept = response[0]
-            console.log(response)
+            this.dept = dept[0]
+            console.log(dept[0])
         }
     }
 } 
